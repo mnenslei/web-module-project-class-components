178a7b113c5347aead51772f5731e15ac9adce1d
Michael Enslein <michael_enslein@michaels-mbp.myfiosgateway.com>
2022-01-10 16:57:26 -0500

    working on app and form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,40 @@
 import React from 'react';
 
+import TodoForm from './components/TodoForm';
+
+const todos = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
+
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todos
+    };
+  }
+
+  handleAddTask = (taskName) => {
+    const newTask = {
+      task: taskName,
+      id: Date.now(),
+      completed: false
+    };
+
+    this.setState({
+      ...this.state,
+      todos: [...this.state.todos, newTask]
+    });
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
@@ -8,6 +42,7 @@ class App extends React.Component {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoForm handleAddTask={this.handleAddTask}/>
       </div>
     );
   }
